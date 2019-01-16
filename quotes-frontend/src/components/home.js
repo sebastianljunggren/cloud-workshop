92121efc56749c82e8df596d46f53c4eb4cb4479
André Samuelsson <andresamuelsson94@gmail.com>
2019-01-16 16:57:26 +0100

    Convert cards view to css-grid

--- quotes-frontend/src/components/home.js
+++ quotes-frontend/src/components/home.js
@@ -19,41 +19,48 @@ export default props => {
   );
 
   return (
-    <App>
-      <Container>
+    <Grid>
+      <Header>
         <h1>Quotes</h1>
         <Link to="/quotes/new">
           <Button name="plus-square" size="2x" />
         </Link>
-        <Page
-          activePage={activePage}
-          onNewNumberOfPages={setTotalNumberOfPages}
-        />
-      </Container>
+      </Header>
+      <Page
+        activePage={activePage}
+        onNewNumberOfPages={setTotalNumberOfPages}
+      />
       <Footer>
         <Paginator
           activePage={activePage}
           totalNumberOfPages={totalNumberOfPages}
         />
       </Footer>
-    </App>
+    </Grid>
   );
 };
 
-const App = styled.div`
+const Grid = styled.div`
   text-align: center;
   height: 100%;
   width: 100%;
   display: grid;
-  grid-template-rows: 1fr 1fr auto;
+  grid-template-rows: 1fr auto 1fr;
+  grid-template-columns: repeat(5, 1fr);
   position: absolute;
+  grid-template-areas:
+    "h h h h h"
+    "c c c c c"
+    "f f f f f";
 `;
 
-const Container = styled.div``;
+const Header = styled.div`
+  grid-area: h;
+`;
 
 const Footer = styled.div`
+  grid-area: f;
   text-align: center;
-  grid-row: 3;
   margin: 2em 0;
   padding: 2em 0;
   border-top: 1px dashed grey;
--- quotes-frontend/src/components/quotesView.js
+++ quotes-frontend/src/components/quotesView.js
@@ -2,37 +2,43 @@ import React from "react";
 import styled from "styled-components";
 
 export default ({ page, onQuoteClicked }) => {
+  const createCard = quoteObj => {
+    const Card = quoteObj.quote.length > 100 ? WideCard : BasicCard;
+    return (
+      <Card key={quoteObj.id} onClick={() => onQuoteClicked(quoteObj.id)}>
+        <Quote>{quoteObj.quote}</Quote>
+        <br />
+        <Author>- {quoteObj.author}</Author>
+      </Card>
+    );
+  };
+
   return (
     <QuotesContainer>
-      {page.content &&
-        page.content.map(quoteObj => (
-          <Card key={quoteObj.id} onClick={() => onQuoteClicked(quoteObj.id)}>
-            <Quote>{quoteObj.quote}</Quote>
-            <br />
-            <Author>- {quoteObj.author}</Author>
-          </Card>
-        ))}
+      {page.content && page.content.map(quoteObj => createCard(quoteObj))}
     </QuotesContainer>
   );
 };
 
 const QuotesContainer = styled.div`
-  display: flex;
-  align-items: center;
-  flex-flow: column wrap;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(10em, 1fr));
+  grid-auto-rows: 10em;
   align-content: center;
-  height: 100vh;
+  grid-gap: 1em;
+  padding: 0 2vw;
+  width: 96vw;
+  grid-auto-flow: dense;
+  justify-content: center;
+  align-content: center;
 `;
 
-const Card = styled.div`
-  margin 0.3em;
+const BasicCard = styled.div`
   overflow-wrap: break-word;
   box-shadow: 2px 2px grey
   padding: 0.2em;
   
   border: 1px solid grey;
-  width: 10em;
-  min-height: 10em;
   display: flex;
   flex-grow: auto;
   flex-direction: column;
@@ -47,6 +53,11 @@ const Card = styled.div`
   }
   
 `;
+
+const WideCard = styled(BasicCard)`
+  grid-column: span 2;
+`;
+
 const Quote = styled.span`
   max-width: 40em;
   display: block;
